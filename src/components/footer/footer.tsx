e85580839c38cf17c4dc4659102c88bf0039909b
Constantine <Okonemmanuelmma@gmail.com>
2025-03-29 20:19:20 +0100

    footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { SlSocialFacebook } from "react-icons/sl";
+import { LuInstagram } from "react-icons/lu";
+import { CiLinkedin } from "react-icons/ci";
+import { GrGithub } from "react-icons/gr";
 
 function Footer() {
   return (
@@ -27,7 +30,22 @@ function Footer() {
         <div className="text-[19px] font-[700] leading-[100%] mb-[10px]">
           Socials
         </div>
-        <div></div>
+        <div className="flex items-center mb-[16px] gap-[12px] text-[18px] text0[rgba(35,35,35,1)] font-[500]">
+          <SlSocialFacebook />
+          <p className="inline">Facebook</p>
+        </div>
+        <div className="flex items-center mb-[16px] gap-[12px] text-[18px] text0[rgba(35,35,35,1)] font-[500]">
+          <CiLinkedin />
+          <p className="inline">Linkedin</p>
+        </div>
+        <div className="flex items-center mb-[16px] gap-[12px] text-[18px] text0[rgba(35,35,35,1)] font-[500]">
+          <GrGithub />
+          <p className="inline">Github</p>
+        </div>
+        <div className="flex items-center mb-[16px] gap-[12px] text-[18px] text0[rgba(35,35,35,1)] font-[500]">
+          <LuInstagram />
+          <p className="inline">Instagram</p>
+        </div>
       </div>
       <div className="w-[163px]">
         <div className="text-[19px] font-[700] leading-[100%] mb-[10px]">
