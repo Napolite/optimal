33d742ae737ffe0675ea60fc35cef8bd5355cc27
Constantine <Okonemmanuelmma@gmail.com>
2025-03-29 20:13:08 +0100

    footer: more wip

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -27,6 +27,7 @@ function Footer() {
         <div className="text-[19px] font-[700] leading-[100%] mb-[10px]">
           Socials
         </div>
+        <div></div>
       </div>
       <div className="w-[163px]">
         <div className="text-[19px] font-[700] leading-[100%] mb-[10px]">
@@ -56,7 +57,25 @@ function Footer() {
           Index
         </div>
       </div>
-      <div></div>
+      <div className="w-[405px]">
+        <div className="text-[30.46px] font-[700] leading-[150%] tracking-[0] mb-[10px]">
+          Stay in loop with Rainbow AI
+        </div>
+        <div className="text-[18px] leading-[150%] text-[rgba(35,35,35,1)] font-[500] mb-[16px] w-[394px]">
+          By subscribing you’ll be the first to get the latest news, update and
+          offers Rainbow AI is bringing to the market.
+        </div>
+        <div className="flex border-2 border-black-500 focus-within:border-blue-700 w-[405px] max-w-md h-[64px]">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="flex-1 p-2 text-lg outline-none"
+          />
+          <button className="bg-black text-white font-bold px-4 py-2">
+            Subscribe
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
