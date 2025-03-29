826e368e3fef9814a4b7d62de60d53f19cc457cf
Constantine <Okonemmanuelmma@gmail.com>
2025-03-29 17:04:55 +0100

    becausd I just want to commit

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -13,9 +13,27 @@ function Pricing() {
           <p className="w-[697px] text-[19px] text-[rgba(110,110,110,1)] mb-[20px] tracking-[-6%] leading-[156%] mt-[30px]">
             Get the best plan that suites your needs
           </p>
-          <div className=" w-[222.86px] h-[46.39px] ring-1 ring-[black] flex justify-between items-center px-[20px] mt-[30px]">
-            <button onClick={() => setClicked("Monthly")}>Monthly</button>
-            <button onClick={() => setClicked("Yearly")}>Yearly</button>
+          <div className=" w-[222.86px] bg-[#9d9d9d10] h-[46.39px] ring-1 ring-[black] flex justify-between items-center px-[9.69px] py-[4.85px] mt-[30px]">
+            <button
+              onClick={() => setClicked("Monthly")}
+              className={`w-[116.4px] h-[36.69px] ${
+                clicked === "Monthly"
+                  ? "bg-[#000000] text-[#ffffff]"
+                  : "bg-none text-[#000000]"
+              }`}
+            >
+              Monthly
+            </button>
+            <button
+              onClick={() => setClicked("Yearly")}
+              className={`w-[116.4px] h-[36.69px] ${
+                clicked === "Yearly"
+                  ? "bg-[#000000] text-[#ffffff]"
+                  : "bg-none text-[#000000]"
+              }`}
+            >
+              Yearly
+            </button>
           </div>
         </div>
         <div>
